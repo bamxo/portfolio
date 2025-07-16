ae4f395bcaeddc331ada04ad2dac16599087d740
Landon Nguyen <110705280+bamxo@users.noreply.github.com>
2025-07-16 03:39:41 -0700

    updated about section
    added:
    - college year
    - tkd club position

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,10 +41,10 @@ const About = () => {
           </div>
           <div className={styles.textContainer}>
             <p className={styles.paragraph}>
-              Hi everyone! My name is Landon, and I'm a third-year Computer Science major at UC Santa Cruz. I have a strong passion for software development, with interests spanning web development, machine learning, and mobile development. I enjoy working on projects where I can see my code turn into something functional and meaningful.
+              Hi everyone! My name is Landon, and I'm a fourth-year Computer Science major at UC Santa Cruz. I have a strong passion for software development, with interests spanning web development, machine learning, and mobile development. I enjoy working on projects where I can see my code turn into something functional and meaningful.
             </p>
             <p className={styles.paragraph}>
-              Beyond coding, I practice Taekwondo and serve as the Demo Team Coordinator for UCSC's Taekwondo Club. I love competing and pushing my limits, both in tournaments and in training. When I'm not working on projects or practicing taekwondo, you'll probably find me snowboarding, eating, or experimenting with new recipes in the kitchen.
+              Beyond coding, I practice Taekwondo and serve as a Co-President for UCSC's Taekwondo Club. I love competing and pushing my limits, both in tournaments and in training. When I'm not working on projects or practicing taekwondo, you'll probably find me snowboarding, eating, or experimenting with new recipes in the kitchen.
             </p>
             <p className={styles.paragraph}>
               I like to create technology that is not only functional and efficient but also impactful and user-friendly. If you're interested in working together, feel free to reach out!
